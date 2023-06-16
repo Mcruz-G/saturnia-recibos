7e8a27e3dba74b49d9a503021d46c74cdb06be69
Mcruz-G <macruzgom@gmail.com>
2023-06-16 16:52:42 -0600

    added saturnia_app.py

---
+++ docaiprocess.py
@@ -0,0 +1,63 @@
+import os
+import json 
+from google.cloud import documentai_v1beta3 as documentai
+
+
+def process_document(file_path: str):
+
+    keys = get_keys()
+    mime_type = 'application/pdf'
+    # Use the built-in Invoice Parser processor
+    project_id, location, processor_id, key_name = keys['project_id'], keys['location'], keys['processor_id'], keys['key_name']
+    
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = f"keys/{key_name}"
+    client = documentai.DocumentProcessorServiceClient()
+
+    # The full resource name of the processor
+    name = f"projects/{project_id}/locations/{location}/processors/{processor_id}"
+
+    with open(file_path, "rb") as image:
+        image_content = image.read()
+
+    document = {"mime_type": mime_type, "content": image_content}
+
+    request = {"name": name, "document": document}
+
+    result = client.process_document(request=request)
+
+    document = result.document
+    print("Document processing complete.")
+    
+    # Extract predicted entities
+    predicted_fields = {}
+    i = 1
+    for entity in document.entities:
+        field_name = entity.type_ 
+
+        if field_name == 'line_item':
+            field_name += f"_{i}"
+            i += 1
+
+        field_value = entity.mention_text
+        predicted_fields[field_name] = field_value
+
+
+    return predicted_fields
+
+def get_text(anchor, document):
+    """
+    Function to locate a layout token given its layout anchor.
+    """
+    response = ''
+    for segment in anchor.segments:
+        start_index = segment.start_index
+        end_index = segment.end_index
+        response += document.text[start_index:end_index]
+    return response.strip()
+
+def get_keys():
+    # Read json file:
+    file_path = 'keys/saturnia-recibos-42d2ad3e0669.json'
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
---
+++ saturnia_app.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import zipfile
+import os
+import pandas as pd
+import shutil
+import base64
+from docaiprocess import process_document  # Import the function you've developed
+from sqlalchemy import create_engine, inspect, text
+
+# Create a SQLAlchemy engine connected to a SQLite database
+engine = create_engine('sqlite:///databases/CFE_Recibos_DB.sqlite')
+
+st.markdown("# Saturnia", unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    h1{
+    font-size:60px !important;
+    color:#6c757d;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
+st.markdown("""
+
+Put here your client's recipts and summarize them into an excel! 
+
+To introduce a .zip file:
+
+1. Click on the 'Browse files' button below.
+2. Navigate to the location of your .zip file in the file dialog.
+3. Select the .zip file and click 'Open'.
+""")
+
+uploaded_file = st.file_uploader("Upload file", type=['zip'])
+
+if os.path.exists('recibos') and os.path.isdir('recibos'):
+    shutil.rmtree('recibos')  # Deletes the directory
+
+if uploaded_file is not None:
+    with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
+        for member in zip_ref.namelist():
+            # This will exclude any directories
+                if not member.endswith('/'):
+                    zip_ref.extract(member, 'recibos')
+
+    fields = {}
+    files = os.listdir('recibos')
+    print(files)
+    with st.spinner('Processing...'):
+        for i, filename in enumerate(files):
+            if filename.endswith('.pdf'):
+                name = filename.split(".")[0]
+                file_path = os.path.join('recibos', filename)
+                result = process_document(file_path)
+                fields[name] = result
+
+    df = pd.DataFrame.from_dict(fields, orient='index')
+    csv = df.to_csv(index=True, index_label="recibo").encode()
+
+    st.success('Done!')
+    st.download_button(label="Download Results as CSV", data=csv, file_name='recibos_data.csv', mime='text/csv')
+
+    inspector = inspect(engine)
+    with engine.connect() as connection:
+        if 'recibos' not in inspector.get_table_names():
+            df.to_sql('recibos', connection, index=True, index_label="recibo")
+        else:
+                existing_ids = pd.read_sql('SELECT recibo FROM recibos', connection)
+                df_new = df[~df.index.isin(existing_ids.index)]
+                if not df_new.empty:
+                    df_new.to_sql('recibos', engine, index=True, index_label='recibo', if_exists='append')
+
+    
+        result = connection.execute(text("SELECT DISTINCT * FROM recibos LIMIT 10"))
+        df = pd.DataFrame(result.fetchall(), columns=result.keys())
+
+        # Query the database to get the first 10 rows from 'recibos' table
+        
+        st.markdown('Collect your clients data into your database. Until now, these are the first 10 rows that you have collected so far:')
+        st.dataframe(df)
