dc6950f21fdd1c4c505c4aea6885040043523e04
Mcruz-G <macruzgom@gmail.com>
2023-07-01 11:54:40 -0600

    Added saturniaApp

--- saturniaApp/backend/app/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('saturnia', __name__, url_prefix='/saturnia')
--- saturniaApp/backend/app/utils/save_data_to_db.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sqlalchemy import inspect, text
-
-def save_data_to_db(engine, df):
-    with engine.connect() as connection:
-        print(df.head())
-        df.to_sql('recibos', connection, index=True, index_label='recibo', if_exists='append')
-        return df
