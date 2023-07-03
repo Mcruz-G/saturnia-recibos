23b49dbb5c6522c1443cfbaa5ca933b4bec10356
Mcruz-G <macruzgom@gmail.com>
2023-07-02 18:56:49 -0600

    .

--- saturnia/app.py
+++ saturnia/app.py
@@ -7,10 +7,11 @@ app = Flask(__name__)
 app.secret_key = '123'  # Set a secret key
 
 app.register_blueprint(upload)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
 
-    app.run(debug=False, )
+    app.run()
