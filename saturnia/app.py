9e2bba0ce6427be36154f79df914e66ef16ed0cc
Mcruz-G <macruzgom@gmail.com>
2023-07-02 20:28:06 -0600

    .

--- saturnia/app.py
+++ saturnia/app.py
@@ -11,10 +11,10 @@ app.register_blueprint(upload)
 @app.route('/')
 def home():
     #Get absolute path to pdfs
-    # pdfs_path = os.path.join(os.getcwd(), 'pdfs')
-    # if os.path.exists(pdfs_path):
-    #     shutil.rmtree(pdfs_path)
-    #     os.mkdir(pdfs_path)
+    pdfs_path = os.path.join(os.getcwd(), 'pdfs')
+    if os.path.exists(pdfs_path):
+        shutil.rmtree(pdfs_path)
+        os.mkdir(pdfs_path)
 
     return render_template('index.html')
 
