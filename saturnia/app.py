c9963245d945dc31624f8b3b666caca1bed4223e
Mcruz-G <macruzgom@gmail.com>
2023-07-02 20:22:23 -0600

    .

--- saturnia/app.py
+++ saturnia/app.py
@@ -5,16 +5,16 @@ import os, shutil
 
 app = Flask(__name__)
 app.secret_key = '123'  # Set a secret key
-
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'pdfs')
 app.register_blueprint(upload)
 
 @app.route('/')
 def home():
     #Get absolute path to pdfs
-    pdfs_path = os.path.join(os.getcwd(), 'pdfs')
-    if os.path.exists(pdfs_path):
-        shutil.rmtree(pdfs_path)
-        os.mkdir(pdfs_path)
+    # pdfs_path = os.path.join(os.getcwd(), 'pdfs')
+    # if os.path.exists(pdfs_path):
+    #     shutil.rmtree(pdfs_path)
+    #     os.mkdir(pdfs_path)
 
     return render_template('index.html')
 
